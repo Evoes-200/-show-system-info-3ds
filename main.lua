d989b694ac435e5ae85a36a118ace69115835c3b
Evoes-200 <121514426+Evoes-200@users.noreply.github.com>
2023-04-14 16:08:26 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,31 @@
-function love.draw()
-    love.graphics.print('Hello World!', 200, 120)
+
+luma = love.graphics.newImage("luma.png")
+
+	
+
+	
+  
+function love.draw(screen)
+    if screen ~= "left" then
+        
+        love.graphics.draw(luma, 20, 0)
+      
+    end
+   
+
+    if screen ~= "bottom" then
+       
+        love.graphics.setColor( 6, 255, 231 )
+        love.graphics.print("Model   "..love.system.getModel(), 0, 10)
+        love.graphics.print("User    "..love.system.getUsername(), 0, 40)
+        love.graphics.print("Region  "..love.system.getRegion(), 0, 70)
+        love.graphics.print(os.date("date  %x %X "), 0, 100)
+        love.graphics.print("Firmware "..love.system.getVersion(), 0, 130)
+        love.graphics.print("developped by Pochon ", 0, 150)
+        
+        
+        
+    end
+    
 end
 
--- we need to quit the app when a button is pressed
-function love.gamepadpressed(joystick, button)
-    love.event.quit()
-end
